10d5a2895f6ff683297cd3a66607cd9d251e3dbd
sai2001-charanteja <66239451+sai2001-charanteja@users.noreply.github.com>
2021-05-21 08:41:10 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,533 @@
+<head>
+	<title>
+		HELLO WORLDS
+	</title>
+	 <!--Import Google Icon Font-->
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+	 <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <style>
+    	h5{
+    		padding: 10px;
+    	}
+    	.box{
+    		/*padding: 10px;*/
+    		width: 200px;
+    	}
+    	.row
+    	{
+    		padding: 10px;
+    	}
+    	.col
+    	{
+    		padding: 10px;
+    	}
+
+    	nav.badge
+    	{
+    		position: relative;
+    		top: 20px;
+    		right: 20px;
+    	}
+
+    </style>
+</head>
+<body>
+
+	<nav>
+		
+
+	</nav>
+
+
+
+
+
+
+
+<!-- 
+
+
+	<div class="container">
+		<h2 >Models</h2>
+		<a href="#terms" class="btn -orange">Terms & conditions</a>
+		<div class="modal" id="terms">
+			<div class="modal-content">
+				<h4>Terms & conditions</h4>
+			</div>
+		</div>
+
+	</div>
+	<script type="">
+		$(document).ready(function()
+		{
+			$('.modal').modal();
+		});
+	</script> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+	<div class="container">
+		<h1>Collections</h1>
+
+		<ul class="collection with-header">
+			<li class="collection-header"><h1>HEllo</h1></li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item avatar">
+				<i class="material-icons circle blue">person</i>
+				<span class="title">chay</span>
+				<p>Heloo chay</p>
+				
+				<a href="#" class="secondary-content">
+					<i class="material-icons blue-text">mail</i>
+				</a>
+
+			</li>
+			<li class="collection-item">Hello</li>
+			<li class="collection-item">Hello</li>
+			<li class="collection-item">Hello</li>
+			<li class="collection-item">Hello</li>
+		</ul>
+
+	</div>
+
+
+
+
+ -->
+
+
+<!-- 
+
+	<div class="conatiner">
+		
+		<div class="row">
+			
+			<div class="col s3 m6 l6">
+				<div class="card">
+					<div class="card-content">
+						<span class="card-title">SAI CHARAN TEJA</span>
+						<p class="red-text flow-text">Hello asdfghjasdfghjksdfghjksdfghjasdfghsdfghjsdfghasdfghasdfghsdfghsdfghjk sdfghjk sdfghjklqwertyuio  </p>
+					</div>
+				</div>
+			</div>
+			<div class="col s3 m6 l6">
+				<div class="card">
+					<div class="card-content">
+						<span class="card-title">SAI CHARAN TEJA</span>
+						<p class="red-text flow-text truncate">Hello asdfghjasdfghjksdfghjksdfghjasdfghsdfghjsdfghasdfghasdfghsdfghsdfghjk sdfghjk sdfghjklqwertyuio  </p>
+					</div>
+				</div>
+			</div>
+			<div class="col s3 m6 l6">
+				<div class="card">
+					<div class="card-content">
+						<span class="card-title">SAI CHARAN TEJA</span>
+						<p class="red-text flow-text">Hello asdfghjasdfghjksdfghjksdfghjasdfghsdfghjsdfghasdfghasdfghsdfghsdfghjk sdfghjk sdfghjklqwertyuio  </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 <!-- <nav class="nav-wrapper blue"></nav> -->
+	<!-- <nav class="nav-wrapper indigo">
+		<div class="container">
+			<a href="#" class="brand-logo red-text">TYPING</a>
+			<a href="#" class="sidenav-trigger" data-target="mobile-links">
+				<i class="material-icons">
+					menu
+				</i>
+			</a>
+			<ul class="right hide-on-med-and-down">
+			<li>
+				<a href="#" class="btn-floating pink darken-4">Home</a>
+			</li>
+
+			<li>
+				<a href="#" class="btn-floating pink darken-4 flow-text">About</a>
+			</li>
+
+			<li>
+				<a href="#" class="btn-floating pink darken-4">
+					<i class="material-icons">phone</i>
+				</a>
+			</li>
+
+			<li>
+				<span class="badge white-text pink">5</span>
+			</li>
+
+			<li>
+				<a href="#" class="btn-floating pink darken-4">login</a>
+			</li>
+		</ul>
+		</div>
+	</nav>
+
+<ul class="sidenav" id="mobile-links">
+			<li>
+				<a href="#">Home</a>
+			</li>
+
+			<li>
+				<a href="#">About</a>
+			</li>
+
+			<li>
+				<a href="#">contact</a>
+			</li>
+
+			<li>
+				<a href="#">login</a>
+			</li>
+</ul> -->
+
+	<!-- <div class="container">
+		<div class="row">
+		<div class="col s2 m2"><div class="z-depth-1"></div></div>
+		<div class="col s2 m2"><div class="z-depth-2"></div></div>
+		<div class="col s2 m2"><div class="z-depth-3"></div></div>
+		<div class="col s2 m2"><div class="z-depth-4"></div></div>
+		<div class="col s2 m2"><div class="z-depth-5"></div></div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+
+    <script >
+    	$(document).ready(function() {
+    		$('.sidenav').sidenav();
+    	})
+    </script>
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="container">
+	<h4>Grid</h4>
+	<div class="row">
+
+		S3 menas 3 columns for small screen and up
+		<div class="col s3">1</div>
+		<div class="col s3 ">2</div>
+		<div class="col s3">3</div>
+		<div class="col s3">4</div>
+		<div class="col s3 truncate">5123456789012345678901234567</div>
+		
+		<div class="col s3">6</div>
+		<div class="col s3">7</div>
+		<div class="col s1">8</div>
+		<div class="col s1">9</div>
+		<div class="col s1">10</div>
+	</div>
+	<div class="row">
+		<div class="col s6 m3">1</div>
+		<div class="col s6 m3">2</div>
+		<div class="col s6 m3">3</div>
+		<div class="col s6 m3">4</div>
+		<div class="col s6">5</div>
+		
+		<div class="col s1">6</div>
+		<div class="col s1">7</div>
+		<div class="col s1">8</div>
+		<div class="col s1">9</div>
+		<div class="col s1">10</div>
+	</div>
+	<div class="row">
+		<div class="col s12 m6 l4">1</div>
+		<div class="col s12 m6 l4">2</div>
+		<div class="col s12 m6 l4">3</div>
+		<div class="col s12 m6 l4">4</div>
+		<div class="col s12 m6 l4">5</div>
+		
+		<div class="col s12 m6 l4">6</div>
+		<div class="col s12 m6 l4">7</div>
+		<div class="col s12 m6 l4">8</div>
+		<div class="col s12 m6 l4">9</div>
+		<div class="col s12 m6 l4">10</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12 m6 l4 xl2">1</div>
+		<div class="col s12 m6 l4 xl2">2</div>
+		<div class="col s12 m6 l4 xl2">3</div>
+		<div class="col s12 m6 l4 xl2">4</div>
+		<div class="col s12 m6 l4 xl2">5</div>
+		
+		<div class="col s12 m6 l4 xl2">6</div>
+		<div class="col s12 m6 l4 xl2">7</div>
+		<div class="col s12 m6 l4 xl2">8</div>
+		<div class="col s12 m6 l4 xl2">9</div>
+		<div class="col s12 m6 l4 xl2">10</div>
+	</div>
+</div>
+
+
+ -->
+
+
+
+
+
+
+
+
+<!-- 
+	USING ICONS
+
+
+	<div class="container">
+		<a href="#" class="btn-large waves-effect indigo pulse">
+			<span>Send</span>
+			<i class="large material-icons left">send</i>
+		</a>
+        <br>
+        <i class="material-icons yellow-text text-darken">add</i>
+        <br>
+        <i class="large material-icons grey-text">insert_chart</i>
+        <br>
+        <i class="material-icons blue">error</i>
+        <br>
+
+        <a class="btn-large pink waves-effect waves-dark" href="#">
+        	<span>Email</span>
+        	<i class="material-icons right">email</i>
+        </a>
+
+        <br>
+        <a href="#" class="btn-floating blue pulse">
+        	<i class="material-icons">add</i>
+        </a>
+
+        <a href="#" class="btn-floating blue pulse">
+        	<i class="material-icons">remove</i>
+        </a>
+
+        <a href="#" class="btn-floating blue pulse">
+        	<i class="material-icons">edit</i>
+        </a>
+
+
+
+	</div>
+
+
+
+
+
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+	BUTTONS
+
+	<div class="container">
+		
+		<h5>BUTTONS</h5>
+		<a class="btn waves-effect waves-light" href="#">Normal Button</a>
+		<a class="btn-small waves-effect waves-dark">Small button with dark waves when we click on it</a>
+		<a class="btn-large">Large Button</a>
+		<a class="btn-small indigo"> INdigo button</a>
+		<a class="btn disabled"> Disabled Button</a>
+		<h5>Floating Button</h5>
+		<a class="btn-floating pink pulse">Pulse grabs our attention</a>
+		<h5>Other Elemnts</h5>
+		<a href="#" class="btn-large"></a>
+
+
+	</div>
+ -->
+
+
+
+<!-- 
+ALlignments 
+
+	<div class="container">
+		<h5 class="center-align"> CENTER ALIGNMENT</h5>
+		<h5 class="left-align">LEft alignment</h5>
+		<h5 class="right-align">RIGHT ALIGMNETN</h5>
+	</div> 
+
+	<div class="container grey lighten-4 box valign-wrapper">
+		<h5>VERTICAL ALIGNMENT  using valign wrapper</h5>
+	</div>
+
+	<div class="container">
+		<p class="truncate">
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+
+
+			SHORTENS THE TEXT
+		</p>
+	</div>
+
+
+
+ -->
+
+
+
+
+
+
+
+<!-- 
+
+backgound color and textr color
+
+	<div class="container">
+		
+		<h5 class="light-green accent-3">defalut color </h5>
+		<h5 class="yellow lighten-4 red-text">LIghten-4 lightends the yellow  wirh red text</h5>
+		<h5 class="blue darken-4 white-text"> blue darken 4 with white text </h5>
+		<h5 class="blue lighten-3 orange-text text-lighten-1 "> blue darken 4 with orangr text and text us lighten </h5>
+		<h5 class="blue lighten-3 orange-text text-darken-3 "> blue darken 4 with orangr text and text us darken </h5>
+	</div> -->
+
+	<!-- 
+	hiding property
+	<div class="container">
+		
+		<h4 class="hide">Hide me on all screen sizes</h4>
+		<h4 class="hide-on-small-only">Hide me on small screen sizes</h4>
+		<h4 class="hide-on-med-only">Hide me on medium screen sizes</h4>
+		<h4 class="hide-on-large-only">Hide me on large screen sizes</h4>
+		<h4 class="hide-on-med-and-down">Hide me on medium &small screen sizes</h4>
+		<h4 class="hide-on-med-and-up">Hide me on medium and large screen sizes</h4>
+	</div>
+
+ -->
+
+
+
+
+
+	<!--
+
+	container class
+	 <div class="container flow-text">
+		<h1>HELLO WORLD</h1>
+		<h2>HELLO WORLD</h2>
+		<h3>HELLO WORLD</h3>
+		<h4>HELLO WORLD</h4>
+		<h5>HELLO WORLD</h5>
+		<h6>HELLO WORLD</h6>
+		<h>HELLO WORLD</h>
+		<p class="flow-text">HELLO WORLDnkjkids,mnkj</p>
+	</div> 
+-->
+
+
